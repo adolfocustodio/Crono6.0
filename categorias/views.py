889c9980d9f6832aacbd411ccf93883a8e004cab
adolfocustodio <adolfocustodio14@gmail.com>
2023-12-27 09:29:27 -0300

    success message

--- categorias/views.py
+++ categorias/views.py
@@ -1,23 +1,27 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views import generic
 from .models import Categoria
 from .forms import CategoriaForm
 from django.urls import reverse_lazy
 
-class CategoriaCriar(generic.CreateView):
+class CategoriaCriar(SuccessMessageMixin, generic.CreateView):
     form_class = CategoriaForm
     model = Categoria
     success_url = reverse_lazy('categorias:categoria_listar')
+    success_message = "A categoria foi criada com sucesso."
 
 class CategoriaListar(generic.ListView):
     model = Categoria
     context_object_name = 'categorias'
     paginate_by = 10
 
-class CategoriaEditar(generic.UpdateView):
+class CategoriaEditar(SuccessMessageMixin, generic.UpdateView):
     form_class = CategoriaForm
     model = Categoria
     success_url = reverse_lazy('categorias:categoria_listar')
+    success_message = "A categoria foi atualizada com sucesso."
 
-class CategoriaExcluir(generic.DeleteView):
+class CategoriaExcluir(SuccessMessageMixin, generic.DeleteView):
     model = Categoria
     success_url = reverse_lazy('categorias:categoria_listar')
+    success_message = "A categoria foi excluída com sucesso."
--- postagens/views.py
+++ postagens/views.py
@@ -1,23 +1,27 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views import generic
 from .models import Postagem
 from .forms import PostagemForm
 from django.urls import reverse_lazy
 
-class PostagemCriar(generic.CreateView):
+class PostagemCriar(SuccessMessageMixin, generic.CreateView):
     form_class = PostagemForm
     model = Postagem
     success_url = reverse_lazy('postagens:postagem_listar')
+    success_message = "A postagem foi criada com sucesso."
 
 class PostagemListar(generic.ListView):
     model = Postagem
     context_object_name = 'postagens'
     paginate_by = 10
 
-class PostagemEditar(generic.UpdateView):
+class PostagemEditar(SuccessMessageMixin, generic.UpdateView):
     form_class = PostagemForm
     model = Postagem
     success_url = reverse_lazy('postagens:postagem_listar')
+    success_message = "A postagem foi atualizada com sucesso."
 
-class PostagemExcluir(generic.DeleteView):
+class PostagemExcluir(SuccessMessageMixin, generic.DeleteView):
     model = Postagem
     success_url = reverse_lazy('postagens:postagem_listar')
+    success_message = "A postagem foi excluída com sucesso."
